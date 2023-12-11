a2ce971eab29145939e40951b7c7d0c06370f5db
Vijay Anand G <vijay.geddada@yahoo.com>
2023-12-10 21:41:03 -0500

    Fix cross_val errors

--- Score_Algorithms.py
+++ Score_Algorithms.py
@@ -87,15 +87,15 @@ X = array[:,0:4]
 Y = array[:,4]
 
 
-# In[49]:
+# In[71]:
 
 
 validation_size = 0.2
 seed = 6
-X_train,Y_train,X_test,Y_test = model_selection.train_test_split(X,Y,test_size = validation_size, random_state=seed)
+X_train,X_test,Y_train,Y_test = model_selection.train_test_split(X,Y,test_size = validation_size, random_state=seed)
 
 
-# In[58]:
+# In[72]:
 
 
 scoring = 'accuracy'
@@ -107,17 +107,12 @@ models.append(('KNN',KNeighborsClassifier()))
 models.append(('CART',DecisionTreeClassifier()))
 models.append(('NB',GaussianNB()))
 models.append(('SVM',SVC()))
-
-
-# In[ ]:
-
-
 results = []
 names = []
-seed = 6
+
 for name,model in models:
-    kfold = model_selection.KFold(n_splits = 10, shuffle=True, random_state = seed)
-    cv_results = model_selection.cross_val_score(model,X_train,Y_train,cv=kfold,scoring=scoring)
+    kfold = model_selection.KFold(n_splits=10, shuffle = True, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
     msg = "%s: %f (%f)" % (name,cv_results.mean(),cv_results.std())
